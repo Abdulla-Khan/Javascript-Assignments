e88e0e30fde1e714a8364dec95d1e558694d8bf0
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-08-31 03:20:09 +0500

    'Assignment-1

---
+++ JS 1/app.js
@@ -0,0 +1,8 @@
+alert('Welcome to my Website')
+
+
+alert('Error! Please Enter a Valid Password')
+
+
+alert('Welcome to JS Land')
+alert('Happy Coding \r\n Prevent this page from creating additional dialogues')
