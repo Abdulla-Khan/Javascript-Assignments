7590e568c12503e98da4668a7eee0c050c981d73
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-09-01 02:03:38 +0500

    'Commit;

---
+++ JS2/app.js
@@ -0,0 +1,46 @@
+var username;
+var myName = 'Muhammad Abdullah Khan'
+
+// var namE = prompt("Enter your name",'')
+// var age = prompt('Enter your Age','')
+// var profession = prompt('Enter your profession','')
+
+// alert(namE)
+// alert('You are '+age+' years old')
+// alert(profession)
+
+var pizza  = 'pizza'
+
+alert(
+    pizza+'\r\n'+
+    pizza.slice(0,-1)+'\r\n'+
+    pizza.slice(0,-2)+'\r\n'+
+    pizza.slice(0,-3)+'\r\n'+
+    pizza.slice(0,-4),
+
+
+    )
+
+
+
+var email = 'abdullahkhan.smiu@gmail.com'
+alert('My email is '+ email)
+var book = 'A smarter way to learn JavaScript'
+
+alert('Learning JS by book '+book)
+
+
+
+
+
+alert(
+    '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+)
+
+
+
+
+document.write(
+
+    '<h1> Yeah I can write html in JS </h1>'
+)
