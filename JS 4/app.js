e09ea9ff8e54acba178c78e7d37e784637fcbdc4
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-09-07 02:31:19 +0500

    Assignment 2

---
+++ JS 4/app.js
@@ -0,0 +1,40 @@
+var [a, b, c] = [1, 2, 3]
+
+
+
+
+// //Declaring Illegal Variables
+
+// var 1w = 'illegal'
+// var .w = 'Illegal'
+// var * s = 'illegal'
+// var alert = 'illegal'
+// // var while = ''
+// var for = 'illegal'
+// //comment for variable and error on line 18 will be removed
+
+// //Declaring Legal Variables
+
+// var a = 'legal'
+// var _d = 'legal'
+// var $e = 'legal'
+// var g1 = 'legal'
+// var v_q = 'legal'
+
+
+
+alert("Value of 1st Variable " + a + " ,2nd Variable " + b + " ,3rd Variable " + c)
+
+
+
+
+document.write('<h1> Rules for writing JS Variables</h1>')
+
+document.writeln('Variable names can only contain numbers, $ and _. For eg:$my_1stvariable<br>')
+
+
+document.writeln('Variable must begin whit letter, $ and _. For eg:$my_1stvariable<br>')
+
+document.writeln('Variable names are case sensitive<br>')
+
+document.writeln('Variable names should not be JS Keywords<br>')
