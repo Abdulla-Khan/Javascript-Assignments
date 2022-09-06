c3beb1081e3068a0437c64ad909dd0fb1d72884a
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-09-07 01:38:32 +0500

    Assignment Added

---
+++ JS 3/app.js
@@ -0,0 +1,14 @@
+var age = 18
+
+alert('I am ' + age + " years old")
+
+
+
+var visit = 14
+alert('You have visited this site ' + visit + ' times')
+
+
+var birthyear = 2003
+
+document.write('<h2>My birth year is ' + birthyear + '</h2>')
+document.writeln('<h2>Data type of declared variable is number</h2>')
